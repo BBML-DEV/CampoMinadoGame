c4da89a5bb6c6ef34737fb7e2d3a30faba26eeb5
Paulo Vitor <pv.marques2010@hotmail.com>
2023-10-25 18:44:11 -0300

    Realizando teste
    Realizando alguns teste unitários para os métodos da classe campo

--- test/br/com/paulo/cm/modelo/CampoTeste.java
+++ test/br/com/paulo/cm/modelo/CampoTeste.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 
+import br.com.paulo.cm.excecao.ExplosaoException;
+
 class CampoTeste {
 	
 	private Campo campo = new Campo(3, 3);
@@ -39,6 +41,70 @@ class CampoTeste {
 		Campo vizinho = new Campo(4,2);
 		boolean resultado = campo.adicionarVizinho(vizinho);
 		assertTrue(resultado);
+	}
+	
+	@Test
+	void testeAlternarMarcacao() {
+		campo.alternarMarcacao();
+		assertTrue(campo.isMarcado());
+	}
+	
+	@Test
+	void testeAlternarMarcacaoDuasVezes() {
+		campo.alternarMarcacao();
+		campo.alternarMarcacao();
+		assertFalse(campo.isMarcado());
+	}
+	
+	@Test
+	void testeAbrirCampoNaoMinadoNaoMarcado() {
+		campo.abrirCampo();
+		assertTrue(campo.isAberto());
+	}
+	
+	@Test
+	void testeAbrirCampoNaoMinadoMarcado() {
+		campo.alternarMarcacao();
+		assertFalse(campo.abrirCampo());
+	}
+	
+	@Test
+	void testeAbrirCampoMinadoMarcado() {
+		campo.minarCampo();
+		campo.alternarMarcacao();
+		assertFalse(campo.abrirCampo());
+	}
+	
+	@Test
+	void testeAbrirCampoMinadoNaoMarcado() {
+		campo.minarCampo();
+		
+		assertThrows(ExplosaoException.class, () -> {
+			campo.abrirCampo();
+		});
+	}
+	
+	@Test
+	void testeCampoMinado() {
+		campo.isMinado();
+		assertFalse(campo.isMinado());
+	}
+	
+	@Test
+	void testeAbrirCampoComVizinhos() {
+		
+		Campo vizinhoDoVizinho1 = new Campo(1, 1);
+		
+		Campo vizinho1 = new Campo(2, 2);
+		vizinho1.adicionarVizinho(vizinhoDoVizinho1);
+		
+		campo.adicionarVizinho(vizinho1);
+		
+		campo.abrirCampo();
+		
+		assertTrue(vizinho1.isAberto() && vizinhoDoVizinho1.isAberto());
+		
+		
 	}
 
 }
